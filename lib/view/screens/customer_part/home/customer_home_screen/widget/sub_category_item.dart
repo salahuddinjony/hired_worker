4ba0bfaebda23cab9ada4e2cca85032acff80504
NishadMiah <moonjur8@gmail.com>
2025-06-26 15:00:40 +0600

    wer

--- lib/view/screens/customer_part/home/customer_home_screen/widget/sub_category_item.dart
+++ lib/view/screens/customer_part/home/customer_home_screen/widget/sub_category_item.dart
@@ -4,12 +4,7 @@ import 'package:get/get.dart';
 import 'package:servana/core/app_routes/app_routes.dart';
 import 'package:servana/utils/app_colors/app_colors.dart';
 import 'package:servana/utils/app_const/app_const.dart';
-import 'package:servana/utils/app_icons/app_icons.dart';
-import 'package:servana/utils/app_images/app_images.dart';
-import 'package:servana/view/components/custom_image/custom_image.dart';
-import 'package:servana/view/components/custom_netwrok_image/custom_network_image.dart';
 import 'package:servana/view/components/custom_text/custom_text.dart';
-import 'package:servana/view/screens/customer_part/home/controller/home_controller.dart';
 import 'package:servana/view/screens/customer_part/home/customer_home_screen/widget/custom_popular_services_card.dart';
 
 class SubCategoryItem extends StatelessWidget {
