d889a0af239c9f6beed56de2921e8051d587484e
sh4on <shaonx@proton.me>
2025-11-06 14:36:50 +0600

    add rating system

--- lib/view/screens/contractor_part/home/on_going_screen/on_going_finish_screen.dart
+++ lib/view/screens/contractor_part/home/on_going_screen/on_going_finish_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:servana/utils/app_colors/app_colors.dart';
@@ -11,27 +12,18 @@ import '../../../../components/custom_royel_appbar/custom_royel_appbar.dart';
 import '../home_screen/home_screen.dart';
 import 'controller/on_going_controller.dart';
 
-class OnGoingFinishScreen extends StatelessWidget {
+class OnGoingFinishScreen extends GetView<OnGoingController> {
   const OnGoingFinishScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final int index = Get.arguments['id'];
     final String name =
-        Get.find<OnGoingController>()
-            .onGoingBookingList[index]
-            .customerId
-            ?.fullName ??
-        " - ";
+        controller.onGoingBookingList[index].customerId?.fullName ?? " - ";
     final String image =
-        Get.find<OnGoingController>()
-            .onGoingBookingList[index]
-            .customerId?.img ??
-        "";
-    final Object price = Get.find<OnGoingController>()
-            .onGoingBookingList[index]
-            .totalAmount ??
-        " - ";
+        controller.onGoingBookingList[index].customerId?.img ?? "";
+    final Object price =
+        controller.onGoingBookingList[index].totalAmount ?? " - ";
 
     return Scaffold(
       appBar: CustomRoyelAppbar(leftIcon: true, titleName: "Finish".tr),
@@ -81,6 +73,68 @@ class OnGoingFinishScreen extends StatelessWidget {
             color: AppColors.textCLr,
             bottom: 10,
           ),
+
+          Obx(() {
+            return TextButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder:
+                      (context) => AlertDialog(
+                        title: const Text("Rate this customer"),
+                        content: RatingBar.builder(
+                          initialRating: 5,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: const EdgeInsets.symmetric(
+                            horizontal: 4.0,
+                          ),
+                          itemBuilder:
+                              (context, _) =>
+                                  const Icon(Icons.star, color: Colors.amber),
+                          onRatingUpdate: (rating) {
+                            controller.rating.value = rating;
+                          },
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Get.back();
+                              controller.rateCustomer(
+                                controller
+                                    .onGoingBookingList[index]
+                                    .customerId!
+                                    .id!,
+                              );
+                            },
+                            child: const Text("Submit"),
+                          ),
+                        ],
+                      ),
+                );
+              },
+              child: Text(
+                controller.statusForRating.value.isLoading
+                    ? 'Submitting...'
+                    : 'Rate Now',
+                style: TextStyle(color: AppColors.primary, fontSize: 18.sp),
+              ),
+            );
+          }),
+
+          CustomText(
+            top: 10.h,
+            text: "or",
+            fontSize: 16.w,
+            fontWeight: FontWeight.w400,
+            color: AppColors.textCLr,
+            bottom: 10,
+          ),
+
+          SizedBox(height: 14.h),
+
           CustomButton(
             width: 180.w,
             height: 40.h,
